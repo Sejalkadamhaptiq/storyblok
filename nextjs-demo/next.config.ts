586e020217ddcb0828bc588f65d228198cc6329b
Sejal Kadam <sejal.kadam@haptiq.com>
2025-09-13 05:06:11 +0530

    Initial commit

--- nextjs-demo/next.config.ts
+++ nextjs-demo/next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // Match all routes
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOWALL', // Allow iframe embedding
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors *", // Allow any origin to embed
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
